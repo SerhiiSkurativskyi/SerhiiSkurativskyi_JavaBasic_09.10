10575d55e2d43f26af8bea95bc9d4647a6a9ef4c
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2022-10-09 22:30:46 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My coordinates");
+        double latitude = 50.425092;
+        double longitude = 30.601822;
+        System.out.println(latitude);
+        System.out.println(longitude);
+        System.out.println("Test3");
+
+    }
+}
