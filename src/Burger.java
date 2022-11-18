7ac159eec906cd92a996d555228e8bed2e341900
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2022-11-18 22:59:36 +0200

    added third constructor

--- src/Burger.java
+++ src/Burger.java
@@ -1,27 +1,38 @@
 public class Burger {
     String bread;
-    String meat;
+    String meat1;
+    String meat2;
     String greens;
     String mayonnaise;
     String cheese;
 
-    public Burger(String bread, String meat, String greens, String mayonnaise, String cheese) {
+    public Burger(String bread, String meat1, String greens, String mayonnaise, String cheese) {
         this.bread = bread;
-        this.meat = meat;
+        this.meat1 = meat1;
         this.greens = greens;
         this.mayonnaise = mayonnaise;
         this.cheese = cheese;
+        System.out.println("Состав стандартного бургера: " + bread + ", " + meat1 + ", " + greens + ", " + mayonnaise + ", " + cheese);
     }
 
-    public Burger(String bread, String meat, String greens, String cheese) {
+    public Burger(String bread, String meat1, String greens, String cheese) {
         this.bread = bread;
-        this.meat = meat;
+        this.meat1 = meat1;
         this.greens = greens;
         this.cheese = cheese;
+        System.out.println("Состав диетического бургера: " + bread + ", " + meat1 + ", " + greens + ", " + cheese);
     }
 
-
-
+    public Burger(String bread, String meat1, String meat2, String greens, String mayonnaise, String cheese) {
+        this.bread = bread;
+        this.meat1 = meat1;
+        this.meat2 = meat2;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
+        this.cheese = cheese;
+        System.out.println("Состав двойного бургера: " + bread + ", " + meat1 + ", " + meat2 + ", " + greens + ", " + mayonnaise + ", " + cheese);
+    }
 }
+
 
 
--- src/BurgerMain.java
+++ src/BurgerMain.java
@@ -1,10 +1,7 @@
 public class BurgerMain {
     public static void main(String[] args) {
-Burger standardBurger= new Burger("булочка","мясо","зелень","майонез","сыр");
-        System.out.println("Состав стандартного бургера: "+ standardBurger.bread+", "+standardBurger.meat+", "+standardBurger.greens+", "+standardBurger.mayonnaise+", "+standardBurger.cheese);
-Burger dietBurger= new Burger("булочка","мясо","зелень","сыр");
-        System.out.println("Состав диетического бургера: "+ dietBurger.bread+", "+dietBurger.meat+", "+dietBurger.greens+", "+dietBurger.cheese);
-Burger doubleBurger= new Burger("булочка","двойное мясо","зелень","майонез","сыр");
-        System.out.println("Состав двойного бургера: "+ doubleBurger.bread+", "+doubleBurger.meat+", "+doubleBurger.greens+", "+doubleBurger.mayonnaise+", "+doubleBurger.cheese);
+        Burger standardBurger = new Burger("булочка", "мясо", "зелень", "майонез", "сыр");
+        Burger dietBurger = new Burger("булочка", "мясо", "зелень", "сыр");
+        Burger doubleBurger = new Burger("булочка", "мясо", "дополнительное мясо", "зелень", "майонез", "сыр");
     }
 }
