15afda2d25c28946221c90a54df50f10dc957370
Serhii Skurativskyi <serhii.skurativskyi@gmail.com>
2022-11-20 12:09:49 +0200

    changed from constructor to methode

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        Person person1=new Person("Alain","Delon","Paris",445131384);
-        Person person2=new Person("Jean","Reno","Los Angeles",645645615);
-        Person person3=new Person("Zinedine","Zidane","Madrid",1817468167);
+        Person person1=new Person();
+        person1.name = "Jean Cloud";
+        person1.surname ="Van Damme";
+        person1.city = "Bila Tserkva";
+        person1.telNumber = 1231354697;
+        person1.personInfo();
+
+        Person person2=new Person();
+        person2.name = "Elon";
+        person2.surname ="Mask";
+        person2.city = "Mars";
+        person2.telNumber = 1515151515;
+        person2.personInfo();
+
+        Person person3=new Person();
+        person3.name = "Dwayne";
+        person3.surname ="Johnson";
+        person3.city = "Honolulu";
+        person3.telNumber = 987456123;
+        person3.personInfo();
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,10 @@
 public class Person {
-    public  String name;
-    public String surname;
-    public String city;
-    public long telNumber;
+    String name;
+    String surname;
+    String city;
+    long telNumber;
 
-      public Person(String name, String surname, String city, long telNumber) {
-        this.name=name;
-        this.surname=surname;
-        this.city=city;
-        this.telNumber=telNumber;
+    void personInfo() {
         System.out.println("Позвонить гражданину " + name + " " + surname + " из города " + city + " можно по номеру " + telNumber);
     }
 }
